73d636b4bd4f54dcfc4cd84cea4464bbb5b71524
Dekoder-py <dekod3r.py@gmail.com>
2025-10-25 19:59:53 +1300

    final commit because its ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import music from "./assets/music.mp3";
 
 function playIt() {
-  new Audio(music).play()
+  new Audio(music).play();
 }
 
 function App() {
   return (
-    <div id="App">
+    <div id="App" className="h-screen flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold">
-        <button className="font-underline" onClick={playIt}>Click for a trick (or a treat)</button>
+        <button className="font-underline" onClick={playIt}>
+          Trick or Treat!
+        </button>
       </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
